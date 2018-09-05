ee6e596bed83ae5f892e76ac65b44713e1e49516
raghavgrover21 <raghav.grover2121@gmail.com>
2018-09-06 01:26:04 +0530

    delegates added

---
+++ Delegates1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+    namespace New_folder__2_
+                                           
+ {                                         
+     public delegate void del_fun1(string str);
+    public class Program
+    {   
+        public static void Main(string[] args)
+        {                                   
+           del_fun1 ob1 = new del_fun1(fun1);       //creating the object of the delegate (delegate is type safe function pointer)
+                                                    //type safe- same signature as that of the function it is going to point to
+           ob1("welcome to C#");
+        }
+        public static void fun1(string str)
+        {
+            Console.WriteLine(str);
+        }
+    
+    }
+
+}
+
+
