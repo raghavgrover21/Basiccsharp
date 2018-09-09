7778ff5b260d652333541c71b0d0ceb022277df3
raghavgrover21 <raghav.grover2121@gmail.com>
2018-09-09 22:30:31 +0530

    test commit by master

--- Delegates1.cs
+++ Delegates1.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
     {   
         public static void Main(string[] args)
         {                                   
-           del_fun1 ob1 = new del_fun1(fun1);       //creating the object of the delegate (delegate is type safe function pointer)
+           del_fun1 ob1 = new del_fun1(fun1);       //creating the object of the delegate just like a class (delegate is type safe function pointer)
                                                     //type safe- same signature as that of the function it is going to point to
            ob1("welcome to C#");
         }
